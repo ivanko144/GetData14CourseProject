1f3cebb0bfeb67e96dc33c9d9c5fd97e4490b7d0
ivanko144 <ivan.kalyuzhnyy@mail.ru>
2015-05-24 20:47:57 +0100

    script corrections

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-## Load test and train data sets and label them with descriptive variable names (features).
+## 1. Load test and train data sets and label them with descriptive variable names (features).
 #- 'features.txt': List of all features.
 #- 'train/X_train.txt': Train set.
 #- 'test/X_test.txt': Test set.
@@ -12,7 +12,7 @@ x_train_set <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE,
 x_test_set <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE,
                           dec = ".", col.names = features$featureName, colClasses = "numeric")
 
-## Load activity code for each observation and merge codes with descriptive activity names
+## 2. Load activity code for each observation and merge codes with descriptive activity names
 #'activity_labels.txt': Links the class labels with their activity name.
 #- 'test/y_test.txt': Test labels. (activties)
 #- 'train/y_train.txt': Train labels. (activties)
@@ -29,7 +29,7 @@ y_test <- merge(y_test, activity_labels, by = "activityCode")
 y_train <- merge(y_train, activity_labels, by = "activityCode") 
   
 
-## Load subject code for each observation
+## 3. Load subject code for each observation
 #- 'train/subject_train.txt'
 #- 'test/subject_test.txt'
 # Each row identifies the subject who performed the activity for each window sample.
@@ -41,25 +41,19 @@ subject_train <- read.delim("UCI HAR Dataset/train/subject_train.txt", header = 
                       col.names = c("subjectCode"))
 
 
-## Compose total dataset
+## 4. Compose total dataset
 
 train_set <- cbind( subject_train, activityName = y_train[, "activityName"], x_train_set)
 test_set <- cbind( subject_test, activityName = y_test[, "activityName"], x_test_set)
 total_set <- rbind(test_set, train_set, make.row.names = FALSE)
 
 
-## Extract measurements on the mean and standard deviation for each measurement. 
-
-#total_set_colnames <- colnames(total_set)
-#mean_and_std_set <- total_set[, c( 1, 2, grep("mean()", total_set_colnames), grep("std()", total_set_colnames))]
+## 5. Extract measurements on the mean and standard deviation for each measurement. 
 
 mean_and_std_set <- total_set[, c( 1, 2, grep("mean\\(\\)", features$featureName) + 2, grep("std\\(\\)", features$featureName) + 2)]
 
 
-## Create data set with the average of each variable for each activity and each subject
-
-#activity_subject_aggregate <- aggregate(. ~  activityName + subjectCode, data = mean_and_std_set, FUN = mean, na.rm = TRUE)
-#write.table(activity_subject_aggregate, "activity_subject_aggregate.txt", row.name=TRUE)
+## 6. Create data set with the average of each variable for each activity and each subject
 
 aggregate(. ~  activityName + subjectCode, data = mean_and_std_set,
           FUN = mean, na.rm = TRUE) %>%
